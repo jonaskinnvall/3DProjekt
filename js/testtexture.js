dd8847e521dabb87347c5aeda74a11efece5b778
kareemelgindy <elgkareem@gmail.com>
2015-05-25 14:35:57 +0200

    gjort ny bildskit

--- js/testtexture.js
+++ js/testtexture.js
@@ -1,74 +1,77 @@
-var testtexture = function(){
-var color = 0x000000;
+var testtexture = function() {
+  var color = 0x000000;
 
-   // Create your main scene
-   var scene = new THREE.Scene();
+  // Create your main scene
+  var scene = new THREE.Scene();
 
-   // Create your main camera
-   var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // Create your main camera
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
+    0.1, 1000);
 
-   // Create lights
-   var light = new THREE.PointLight(0xEEEEEE);
-   light.position.set(20, 0, 20);
-   scene.add(light);
+  // Create lights
+  var light = new THREE.PointLight(0xEEEEEE);
+  light.position.set(20, 0, 20);
+  scene.add(light);
 
-   var lightAmb = new THREE.AmbientLight(0x777777);
-   scene.add(lightAmb);
+  var lightAmb = new THREE.AmbientLight(0x777777);
+  scene.add(lightAmb);
 
-   // Create your renderer
-   var renderer = new THREE.WebGLRenderer();
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   document.body.appendChild(renderer.domElement);
+  // Create your renderer
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-   // Create a cube
-   var geometry = new THREE.CubeGeometry(1, 1, 1);
-   var material = new THREE.MeshLambertMaterial({
-       color: 0xff00ff,
-       ambient: 0x121212,
-       emissive: 0x121212
-    });
+  // Create a cube
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  var material = new THREE.MeshLambertMaterial({
+    color: 0xff00ff,
+    ambient: 0x121212,
+    emissive: 0x121212
+  });
 
-   var cube = new THREE.Mesh(geometry, material);
-   scene.add(cube);
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-   // Set up the main camera
-   camera.position.z = 5;
+  // Set up the main camera
+  camera.position.z = 5;
 
-   // Load the background texture
-   var texture = THREE.ImageUtils.loadTexture( 'https://linuxradar.files.wordpress.com/2014/06/ih9dnii.png' );
-   var backgroundMesh = new THREE.Mesh(
-       new THREE.PlaneGeometry(2, 2, 0),
-       new THREE.MeshBasicMaterial({
-           map: texture
-       }));
 
-   backgroundMesh .material.depthTest = false;
-   backgroundMesh .material.depthWrite = false;
 
-   // Create your background scene
-   var backgroundScene = new THREE.Scene();
-   var backgroundCamera = new THREE.Camera();
-   backgroundScene .add(backgroundCamera );
-   backgroundScene .add(backgroundMesh );
+  var texture = THREE.ImageUtils.loadTexture(
+    'https://linuxradar.files.wordpress.com/2014/06/ih9dnii.png');
+  var backgroundMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2, 0),
+    new THREE.MeshBasicMaterial({
+      map: texture
+    }));
 
-   // Rendering function
-   var render = function () {
-       requestAnimationFrame(render);
+  backgroundMesh.material.depthTest = false;
+  backgroundMesh.material.depthWrite = false;
 
-       // Update the color to set
-       if (color < 0xdddddd) color += 0x0000ff;
+  // Create your background scene
+  var backgroundScene = new THREE.Scene();
+  var backgroundCamera = new THREE.Camera();
+  backgroundScene.add(backgroundCamera);
+  backgroundScene.add(backgroundMesh);
 
-       // Update the cube color
-       cube.material.color.setHex(color);
+  // Rendering function
+  var render = function() {
+    requestAnimationFrame(render);
 
-       // Update the cube rotations
-       cube.rotation.x += 0.05;
-       cube.rotation.y += 0.02;
+    // Update the color to set
+    if (color < 0xdddddd) color += 0x0000ff;
 
-       renderer.autoClear = false;
-       renderer.clear();
-       renderer.render(backgroundScene , backgroundCamera );
-       renderer.render(scene, camera);
+    // Update the cube color
+    cube.material.color.setHex(color);
+
+    // Update the cube rotations
+    cube.rotation.x += 0.05;
+    cube.rotation.y += 0.02;
+
+    renderer.autoClear = false;
+    renderer.clear();
+    renderer.render(backgroundScene, backgroundCamera);
+    renderer.render(scene, camera);
   };
 
   render();
